43a917291d3c838f24cd7f4d7a5f589b830bec07
DaliaAh <ahmaddalia708@gmail.com>
2023-03-29 14:25:11 -0700

    Run document()

--- R/my_add.R
+++ R/my_add.R
@@ -1,3 +1,17 @@
+#' Add two numbers
+#'
+#' This function adds two numbers and returns the result.
+#'
+#' @param x The first number to add.
+#' @param y The second number to add (default: 10).
+#'
+#' @return The sum of x and y.
+#' @export
+#'
+#' @examples
+#' my_add(1)    # Returns 11, since y defaults to 10
+#' my_add(1, 2) # Returns 3 , since y is set to 2
+#'
 my_add <- function(x, y = 10) {
   if (is.na(x) || is.na(y)) {
     return(NA)
